65f66830bf314d6293edc0f9e25d24d1edef0796
Peter-Immanuel <peterbemshima@gmail.com>
2022-10-04 09:57:41 +0100

    implemented update recipe endpoint

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -28,8 +26,8 @@ var recipes []Recipe
 
 func init() {
 	recipes = make([]Recipe, 0)
-	file, _ := os.ReadFile("recipes.json")
-	_ = json.Unmarshal(file, &recipes)
+	// file, _ := os.ReadFile("recipes.json")
+	// _ = json.Unmarshal(file, &recipes)
 }
 
 func NewRecipeHandler(c *gin.Context) {
@@ -48,9 +46,37 @@ func ListRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipes)
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	var recipe Recipe
+	err := c.ShouldBindJSON(&recipe)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	index := -1
+
+	for i := 0; i <= len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Sorry Recipe not found"})
+		return
+	}
+	c.JSON(http.StatusOK, recipes[index])
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipeHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.Run()
 }
